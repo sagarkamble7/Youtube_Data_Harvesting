6bbb6acdca78aaef8365b252da11bd966c658d18
Sagar Kamble <151813232+sagarkamble7@users.noreply.github.com>
2024-08-24 12:35:01 +0530

    Update youtube_data.py

--- youtube_data.py
+++ youtube_data.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 #API connect:
 
-api_key = "AIzaSyAZ3ZNDTNSd1c6JTLWt3ZWWqngnWX0e7cg"
+api_key = "enter api key here"
 api_service_name = "youtube"
 api_version = "v3"
 youtube = build( api_service_name, api_version, developerKey = api_key )
